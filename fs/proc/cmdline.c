bef6eef786fcf806f2b758ffa9c9b397585ec221
Ameer Azizan <ameerazizan97@gmail.com>
2024-04-18 05:30:37 +0800

    Android 14 bringup

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -4,11 +4,48 @@
 #include <linux/seq_file.h>
 #include <asm/setup.h>
 
-static char new_command_line[COMMAND_LINE_SIZE];
+#if defined(CONFIG_INITRAMFS_IGNORE_SKIP_FLAG) || \
+    defined(CONFIG_PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT)
+#define INITRAMFS_STR_FIND "skip_initramf"
+#endif
+
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+#define INITRAMFS_STR_REPLACE "want_initramf"
+#define INITRAMFS_STR_LEN (sizeof(INITRAMFS_STR_FIND) - 1)
+#endif
+
+#ifdef CONFIG_PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT
+#define ANDROID_FORCE_NORMAL_BOOT_STR "androidboot.force_normal_boot=1"
+#endif
+
+static char proc_command_line[COMMAND_LINE_SIZE];
+
+static void proc_command_line_init(void) {
+	char *offset_addr;
+	char *proc_command_line_tail;
+
+	strcpy(proc_command_line, saved_command_line);
+
+#ifdef CONFIG_INITRAMFS_IGNORE_SKIP_FLAG
+	offset_addr = strstr(proc_command_line, INITRAMFS_STR_FIND);
+	if (offset_addr)
+		memcpy(offset_addr, INITRAMFS_STR_REPLACE, INITRAMFS_STR_LEN);
+#endif
+
+#ifdef CONFIG_PROC_CMDLINE_APPEND_ANDROID_FORCE_NORMAL_BOOT
+	if (strstr(saved_command_line, INITRAMFS_STR_FIND)) {
+		// point proc_command_line_tail to the null terminator of the cmdline
+		proc_command_line_tail = proc_command_line + strlen(proc_command_line);
+		memcpy(proc_command_line_tail, " ", 1);
+		memcpy(proc_command_line_tail + 1, ANDROID_FORCE_NORMAL_BOOT_STR,
+                        sizeof(ANDROID_FORCE_NORMAL_BOOT_STR));
+	}
+#endif
+}
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "%s\n", new_command_line);
+	seq_printf(m, "%s\n", proc_command_line);
 	return 0;
 }
 
@@ -24,37 +61,9 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
-static void remove_flag(char *cmd, const char *flag)
-{
-	char *start_addr, *end_addr;
-
-	/* Ensure all instances of a flag are removed */
-	while ((start_addr = strstr(cmd, flag))) {
-		end_addr = strchr(start_addr, ' ');
-		if (end_addr)
-			memmove(start_addr, end_addr + 1, strlen(end_addr));
-		else
-			*(start_addr - 1) = '\0';
-	}
-}
-
-static void remove_safetynet_flags(char *cmd)
-{
-	remove_flag(cmd, "androidboot.enable_dm_verity=");
-	remove_flag(cmd, "androidboot.secboot=");
-	remove_flag(cmd, "androidboot.verifiedbootstate=");
-	remove_flag(cmd, "androidboot.veritymode=");
-}
-
 static int __init proc_cmdline_init(void)
 {
-	strcpy(new_command_line, saved_command_line);
-
-	/*
-	 * Remove various flags from command line seen by userspace in order to
-	 * pass SafetyNet CTS check.
-	 */
-	remove_safetynet_flags(new_command_line);
+	proc_command_line_init();
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
